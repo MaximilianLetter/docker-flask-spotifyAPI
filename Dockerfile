d3e5d9d0691beb7bd827c23e55882e865a99aba0
MaximilianLetter <maximilian.letter@gmail.com>
2024-01-05 11:46:54 +0100

    Connect with Spotify API with OAuth to show personal playlists

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,14 @@ FROM ubuntu
 
 RUN apt update
 RUN apt install python3-pip -y
+
+RUN pip3 install python-dotenv
 RUN pip3 install Flask
+RUN pip3 install requests
+
 
 WORKDIR /app
 
 COPY . .
 
-CMD [ "python3", "-m", "flask", "run", "--host=0.0.0.0"]
+CMD [ "python3", "app.py"]
--- app.py
+++ app.py
@@ -1,7 +1,137 @@
-from flask import Flask
+# OAuth based off: https://www.youtube.com/watch?v=olY_2MW4Eik
 
+import os
+from dotenv import load_dotenv
+
+import requests
+import urllib.parse
+
+from datetime import datetime, timedelta
+from flask import Flask, redirect, request, jsonify, session
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Setup Flask app
 app = Flask(__name__)
+app.secret_key = os.getenv('FLASK_SECRET')
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+
+AUTH_URL = os.getenv('AUTH_URL')
+TOKEN_URL = os.getenv('TOKEN_URL')
+API_BASE_URL = os.getenv('API_BASE_URL')
 
 @app.route('/')
 def index():
-    return "Hello Docker!"
+    """
+    Index page, without any route specified.
+    """
+    return "Please <a href='/login'>log into Spotify</a> first."
+
+
+@app.route('/login')
+def login():
+    """
+    Login redirecting to Spotify authorization.
+    """
+    scope = 'user-read-private user-read-email'
+
+    params = {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI,
+        'show_dialog': True # false on default, just for testing here
+    }
+
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+
+    return redirect(auth_url)
+
+
+@app.route('/callback')
+def callback():
+    """
+    Callback triggered by Spotify after attempting login.
+    Currently redirecting to /playlists.
+    """
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+
+        response = requests.post(TOKEN_URL, data=req_body)
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+
+        return redirect('/playlists')
+
+
+@app.route('/playlists')
+def get_playlists():
+    """
+    Shows the playlist of the authorized user.
+    """
+    checkAccess(session)
+    
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+
+    response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
+    playlists = response.json()
+
+    return jsonify(playlists)
+
+
+@app.route('/refresh-token')
+def refresh_token():
+    """
+    Refreshes the token given by Spotify if necessary.
+    """
+    checkAccess(session, True, False)
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type': 'referesh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+
+        response = requests.post(TOKEN_URL, data=req_body)
+        new_token_info = response.json()
+
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+
+        return redirect('/playlists')
+
+
+def checkAccess(session, checkLogin = True, checkTimestamp = True):
+    """
+    Checks the access of this session, redirects to login or referesh if required.
+    """
+    if checkLogin:
+        if 'access_token' not in session:
+            return redirect('/login')
+    
+    if checkTimestamp:
+        if datetime.now().timestamp() > session['expires_at']:
+            return redirect('/refresh-token')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
